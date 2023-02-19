ad5e851a9136a7f366ee7150474ef91026e0fbad
Tanjila Shamima <tanjila.cse.diu@gmail.com>
2023-02-19 15:12:42 +0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [value, setValue] = useState<string>();
+
+  useEffect(() => {
+    let array = []
+    let inc = 0
+    while(inc<10){
+      array.push(++inc)
+    }
+    const data = array.join('-')
+    console.log(data)
+    setValue(data)
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {value.map((num, index) => (
+        <li key={index}>{num}</li>
+      ))} */}
+      {value}
     </div>
   );
 }
